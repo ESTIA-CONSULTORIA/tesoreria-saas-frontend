5770755c5f62f17a8fd6f3b605c45802f7618dac
ESTIA-CONSULTORIA <loraloraangel@gmail.com>
2026-06-28 12:42:06 -0700

    debug(executive): log variable de entorno VITE_EXECUTIVE_TENANT_ID

--- src/pages/executive/ExecutiveLogin.tsx
+++ src/pages/executive/ExecutiveLogin.tsx
@@ -12,6 +12,7 @@ const STORAGE_KEY = "exec_tenant_id";
 const KEYS = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "", "0", "del"];
 
 export default function ExecutiveLogin({ onLogin, config }: Props) {
+  console.log("EXEC TENANT ENV:", import.meta.env.VITE_EXECUTIVE_TENANT_ID);
   const t = getTheme(config?.theme || "dark");
   const [brandLogo, setBrandLogo] = useState("");
   const [brandName, setBrandName] = useState("Vista Ejecutiva");
